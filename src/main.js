bacbf88720ff8a2491ea4d8645b1211432a5b820
Nehemiah Torres <nehemiah.torres@ufl.edu>
2019-03-31 18:04:09 -0400

    Add new instance of IceCream to js

--- src/main.js
+++ src/main.js
@@ -5,16 +5,20 @@ class Desserts {
     this.calories = calories;
   }
 }
+// create subclass and add 2 new properties
 class IceCream extends Desserts {
   constructor(types, calories, flavor, scoops) {
     super(types, calories);
     this.flavor = flavor;
     this.scoops = scoops;
   }
-
+// create method in subclass
   includeSpoon() {
     if(this.type === 'Vanilla') {
       console.log('Here is your spoon!');
     }
   }
 }
+
+const vanillaIceCream = new IceCream('IceCream', 340, 'Vanilla', 3);
+  console.log(vanillaIceCream);
